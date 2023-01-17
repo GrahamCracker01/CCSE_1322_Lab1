710e18546d64bc0aa76e32a068d49ee24ecb7932
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2023-01-17 18:30:30 -0500

    Initial commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,2 +1,135 @@
-package PACKAGE_NAME;public class Lab1 {
+//Class: CSE 1322L
+//Section: J03
+//Term: Spring 2023
+//Name: Asher Graham
+
+public class Lab1 {
+
+    public static char[][] make_forward()
+    {
+        char[][] pixel = new char[4][13];
+        pixel[0][0]=' ';
+        pixel[0][1]=' ';
+        pixel[0][2]='_';
+        pixel[0][3]='_';
+        pixel[0][4]='_';
+        pixel[0][5]='_';
+        pixel[0][6]='_';
+        pixel[0][7]='_';
+        pixel[0][8]=' ';
+        pixel[0][9]=' ';
+        pixel[0][10]=' ';
+        pixel[0][11]=' ';
+        pixel[0][12]=' ';
+        pixel[1][0]=' ';
+        pixel[1][1]='/';
+        pixel[1][2]='|';
+        pixel[1][3]='_';
+        pixel[1][4]='|';
+        pixel[1][5]='|';
+        pixel[1][6]='_';
+        pixel[1][7]='\\';
+        pixel[1][8]='\'';
+        pixel[1][9]='.';
+        pixel[1][10]='_';
+        pixel[1][11]='_';
+        pixel[1][12]=' ';
+        pixel[2][0]='(';
+        pixel[2][1]=' ';
+        pixel[2][2]=' ';
+        pixel[2][3]=' ';
+        pixel[2][4]='_';
+        pixel[2][5]=' ';
+        pixel[2][6]=' ';
+        pixel[2][7]=' ';
+        pixel[2][8]=' ';
+        pixel[2][9]='_';
+        pixel[2][10]=' ';
+        pixel[2][11]='_';
+        pixel[2][12]='\\';
+        pixel[3][0]='=';
+        pixel[3][1]='\'';
+        pixel[3][2]='-';
+        pixel[3][3]='(';
+        pixel[3][4]='_';
+        pixel[3][5]=')';
+        pixel[3][6]='-';
+        pixel[3][7]='-';
+        pixel[3][8]='(';
+        pixel[3][9]='_';
+        pixel[3][10]=')';
+        pixel[3][11]='-';
+        pixel[3][12]='\'';
+        return pixel;
+    }
+
+    public static char[][] make_mirror(char[][] array1) {
+        char[][] array2 = new char[4][13];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                int k = (12-j);
+                array2[i][j] = array1[i][k];
+                if (array2[i][j] == '(') {
+                    array2[i][j] = ')';
+                }
+                else if (array2[i][j] == ')') {
+                    array2[i][j] = '(';
+                }
+                else if (array2[i][j] == '/') {
+                    array2[i][j] = '\\';
+                }
+                else if (array2[i][j] == '\\') {
+                    array2[i][j] = '/';
+                }
+            }
+        }
+        return array2;
+    }
+
+    public static void main(String[] args) {
+
+        //create and load array
+        char[][] array1 = new char[4][13];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                array1[i][j] = make_forward()[i][j];
+            }
+        }
+
+        //first print
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                System.out.print(array1[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        //mirror array
+        char[][] array2 = new char[4][13];
+        array2 = make_mirror(array1);
+
+        //second print
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                System.out.print(array2[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        //final print
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 26; j++) {
+                if (j < 13) {
+                    System.out.print(array1[i][j]);
+                }
+                else {
+                    System.out.print(array2[i][j-13]);
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
